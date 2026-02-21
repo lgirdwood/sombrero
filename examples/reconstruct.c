222f6d721e8f15e00ac320e9afb6d9ea5b26683b
Liam Girdwood <lgirdwood@gmail.com>
2026-02-21 11:46:29 +0000

    feat: Add command-line argument parsing for input and output files to the reconstruct example.

--- examples/reconstruct.c
+++ examples/reconstruct.c
@@ -17,61 +17,80 @@
  *
  */
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
+#include <unistd.h>
 
-#include "sombrero.h"
 #include "bmp.h"
+#include "sombrero.h"
 
-#define	SCALES		8
+#define SCALES 8
 
-int main(int argc, char *argv[])
-{
-	struct smbrr *image;
-	struct bitmap *bmp;
-	const void *data;
-	int ret, width, height, stride;
-	enum smbrr_source_type depth;
-	float mean, sigma;
-
-	if (argc == 2)
-		ret = bmp_load(argv[1], &bmp, &data);
-	else
-		ret = bmp_load("images/wiz-ha-x.bmp", &bmp, &data);
-	if (ret < 0)
-		return ret;
-
-	height = bmp_height(bmp);
-	width = bmp_width(bmp);
-	depth = bmp_depth(bmp);
-	stride = bmp_stride(bmp);
-	fprintf(stdout, "Image width %d height %d stride %d\n",
-		width, height, stride);
-
-	image = smbrr_new(SMBRR_DATA_2D_FLOAT, width, height, stride,
-		depth, data);
-	if (image == NULL) {
-		fprintf(stderr, "cant create new image\n");
-		return -EINVAL;
-	}
-
-	mean = smbrr_get_mean(image);
-	sigma = smbrr_get_sigma(image, mean);
-	fprintf(stdout, "Image before mean %f sigma %f\n", mean, sigma);
-
-	smbrr_reconstruct(image, SMBRR_WAVELET_MASK_LINEAR, 1.0e-4, 8,
-		SMBRR_CLIP_VGENTLE);
-
-	mean = smbrr_get_mean(image);
-	sigma = smbrr_get_sigma(image, mean);
-	fprintf(stdout, "Image after mean %f sigma %f\n", mean, sigma);
-
-	bmp_image_save(image, bmp, "reconstruct");
-	free(bmp);
-	smbrr_free(image);
-
-	return 0;
+static void usage(char *argv[]) {
+  fprintf(stdout, "Usage:%s -i infile.bmp -o outfile\n", argv[0]);
+  fprintf(stdout, " -i Input bitmap file - only greyscale supported\n");
+  fprintf(stdout, " -o Output file name\n");
+  exit(0);
 }
 
+int main(int argc, char *argv[]) {
+  struct smbrr *image;
+  struct bitmap *bmp;
+  const void *data;
+  int ret, width, height, stride, opt;
+  enum smbrr_source_type depth;
+  float mean, sigma;
+  char *ifile = NULL, *ofile = NULL;
+
+  while ((opt = getopt(argc, argv, "i:o:")) != -1) {
+    switch (opt) {
+    case 'i':
+      ifile = optarg;
+      break;
+    case 'o':
+      ofile = optarg;
+      break;
+    default: /* '?' */
+      usage(argv);
+    }
+  }
+
+  if (ifile == NULL || ofile == NULL)
+    usage(argv);
+
+  ret = bmp_load(ifile, &bmp, &data);
+  if (ret < 0)
+    return ret;
+
+  height = bmp_height(bmp);
+  width = bmp_width(bmp);
+  depth = bmp_depth(bmp);
+  stride = bmp_stride(bmp);
+  fprintf(stdout, "Image width %d height %d stride %d\n", width, height,
+          stride);
+
+  image = smbrr_new(SMBRR_DATA_2D_FLOAT, width, height, stride, depth, data);
+  if (image == NULL) {
+    fprintf(stderr, "cant create new image\n");
+    return -EINVAL;
+  }
+
+  mean = smbrr_get_mean(image);
+  sigma = smbrr_get_sigma(image, mean);
+  fprintf(stdout, "Image before mean %f sigma %f\n", mean, sigma);
+
+  smbrr_reconstruct(image, SMBRR_WAVELET_MASK_LINEAR, 1.0e-4, 8,
+                    SMBRR_CLIP_VGENTLE);
+
+  mean = smbrr_get_mean(image);
+  sigma = smbrr_get_sigma(image, mean);
+  fprintf(stdout, "Image after mean %f sigma %f\n", mean, sigma);
+
+  bmp_image_save(image, bmp, ofile);
+  free(bmp);
+  smbrr_free(image);
+
+  return 0;
+}
